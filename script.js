66a57bc82f53de94bed672dcec696f55eb3dea8e
DannyTu2 <78064051+DannyTu2@users.noreply.github.com>
2024-10-21 19:35:12 -0400

    Remove comment

--- script.js
+++ script.js
@@ -1,195 +1,252 @@
 function GameBoard() {
-    const rows = 3;
-    const columns = 3;
-    const board = [];
+  const rows = 3;
+  const columns = 3;
+  const board = [];
 
-    for(let i = 0; i < rows; i++) {
-        board[i] = []
-        for(let j = 0; j < columns; j++) {
-            board[i].push(Cell());
-        }
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push(Cell());
+    }
+  }
+
+  const getBoard = () => board;
+
+  const placeToken = (row, column, player) => {
+    const availableCells = board
+      .filter((row) => row[column].getValue() === 0)
+      .map((row) => row[column]);
+    if (!availableCells.length) return;
+
+    board[row][column].addToken(player);
+  };
+
+  const printBoard = () => {
+    const boardWithCellValues = board.map((row) =>
+      row.map((cell) => cell.getValue())
+    );
+    console.log(boardWithCellValues);
+  };
+
+  const winningCondition = () => {
+    // first row horizontal win condition
+    if (
+      board[0][0].getValue() === 1 &&
+      board[0][1].getValue() === 1 &&
+      board[0][2].getValue() === 1
+    ) {
+      console.log("Player 1 is the winner!");
+      return true; // trying to make it so it breaks in the for loop
+    } else if (
+      board[0][0].getValue() === 2 &&
+      board[0][1].getValue() === 2 &&
+      board[0][2].getValue() === 2
+    ) {
+      console.log("Player 2 is the winner!");
+      return true;
+    }
+    // second row horizontal win condition
+    if (
+      board[1][0].getValue() === 1 &&
+      board[1][1].getValue() === 1 &&
+      board[1][2].getValue() === 1
+    ) {
+      console.log("Player 1 is the winner!");
+      return true;
+    } else if (
+      board[1][0].getValue() === 2 &&
+      board[1][1].getValue() === 2 &&
+      board[1][2].getValue() === 2
+    ) {
+      console.log("Player 2 is the winner!");
+      return true;
     }
 
-    const getBoard = () => board;
+    // third row horizontal win condition
+    if (
+      board[2][0].getValue() === 1 &&
+      board[2][1].getValue() === 1 &&
+      board[2][2].getValue() === 1
+    ) {
+      console.log("Player 1 is the winner!");
+      return true;
+    } else if (
+      board[2][0].getValue() === 2 &&
+      board[2][1].getValue() === 2 &&
+      board[2][2].getValue() === 2
+    ) {
+      console.log("Player 2 is the winner!");
+      return true;
+    }
 
-    const placeToken = (row, column, player) => {
-        const availableCells = board.filter((row) => row[column].getValue() === 0).map(row => row[column]);
-        if (!availableCells.length) return;
+    // first vertical win condition
+    if (
+      board[0][0].getValue() === 1 &&
+      board[1][0].getValue() === 1 &&
+      board[2][0].getValue() === 1
+    ) {
+      console.log("Player 1 is the winner");
+      return true;
+    } else if (
+      board[0][0].getValue() === 1 &&
+      board[1][0].getValue() === 1 &&
+      board[2][0].getValue() === 1
+    ) {
+      console.log("Player 2 is the winner");
+      return true;
+    }
 
-        board[row][column].addToken(player);
-      };
+    // second vertical win condition
+    if (
+      board[0][1].getValue() === 1 &&
+      board[1][1].getValue() === 1 &&
+      board[2][1].getValue() === 1
+    ) {
+      console.log("Player 1 is the winner");
+      return true;
+    } else if (
+      board[0][1].getValue() === 1 &&
+      board[1][1].getValue() === 1 &&
+      board[2][1].getValue() === 1
+    ) {
+      console.log("Player 2 is the winner");
+      return true;
+    }
 
-    const printBoard = () => {
-        const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
-        console.log(boardWithCellValues);
-      };
+    // second vertical win condition
+    if (
+      board[0][2].getValue() === 1 &&
+      board[1][2].getValue() === 1 &&
+      board[2][2].getValue() === 1
+    ) {
+      console.log("Player 1 is the winner");
+      return true;
+    } else if (
+      board[0][2].getValue() === 1 &&
+      board[1][2].getValue() === 1 &&
+      board[2][2].getValue() === 1
+    ) {
+      console.log("Player 2 is the winner");
+      return true;
+    }
 
-    const winningCondition = () => {
+    // diagonal 1
+    if (
+      board[0][0].getValue() === 1 &&
+      board[1][1].getValue() === 1 &&
+      board[2][2].getValue() === 1
+    ) {
+      console.log("Player 1 is the winner");
+      return true;
+    } else if (
+      board[0][0].getValue() === 1 &&
+      board[1][1].getValue() === 1 &&
+      board[2][2].getValue() === 1
+    ) {
+      console.log("Player 2 is the winner");
+      return true;
+    }
 
-            // first row horizontal win condition
-            if((board[0][0].getValue() === 1 && board[0][1].getValue() === 1 && board[0][2].getValue() === 1)) {
-                console.log("Player 1 is the winner!")
-                return true; // trying to make it so it breaks in the for loop
-            } else if((board[0][0].getValue() === 2 && board[0][1].getValue() === 2 && board[0][2].getValue() === 2)) {
-                console.log("Player 2 is the winner!");
-                return true;
-            }
-            // second row horizontal win condition
-            if((board[1][0].getValue() === 1 && board[1][1].getValue() === 1 && board[1][2].getValue() === 1)) {
-              console.log("Player 1 is the winner!")
-              return true;
-            } else if ((board[1][0].getValue() === 2 && board[1][1].getValue() === 2 && board[1][2].getValue() === 2)) {
-              console.log("Player 2 is the winner!");
-              return true;
-            }
+    // diagonal 2
+    if (
+      board[2][0].getValue() === 1 &&
+      board[1][1].getValue() === 1 &&
+      board[0][2].getValue() === 1
+    ) {
+      console.log("Player 1 is the winner");
+      return true;
+    } else if (
+      board[2][0].getValue() === 1 &&
+      board[1][1].getValue() === 1 &&
+      board[0][2].getValue() === 1
+    ) {
+      console.log("Player 2 is the winner");
+      return true;
+    }
+  };
 
-            // third row horizontal win condition
-            if((board[2][0].getValue() === 1 && board[2][1].getValue() === 1 && board[2][2].getValue() === 1)) {
-              console.log("Player 1 is the winner!")
-              return true;
-            } else if ((board[2][0].getValue() === 2 && board[2][1].getValue() === 2 && board[2][2].getValue() === 2)) {
-              console.log("Player 2 is the winner!");
-              return true;
-            }
-
-            // first vertical win condition
-            if((board[0][0].getValue() === 1 && board[1][0].getValue() === 1) && board[2][0].getValue() === 1) {
-              console.log("Player 1 is the winner")
-              return true;
-            } else if((board[0][0].getValue() === 1 && board[1][0].getValue() === 1) && board[2][0].getValue() === 1) {
-              console.log("Player 2 is the winner")
-              return true;
-            }
-
-            // second vertical win condition
-            if((board[0][1].getValue() === 1 && board[1][1].getValue() === 1) && board[2][1].getValue() === 1) {
-              console.log("Player 1 is the winner")
-              return true;
-            } else if((board[0][1].getValue() === 1 && board[1][1].getValue() === 1) && board[2][1].getValue() === 1) {
-              console.log("Player 2 is the winner")
-              return true;
-            }
-
-             // second vertical win condition
-            if((board[0][2].getValue() === 1 && board[1][2].getValue() === 1) && board[2][2].getValue() === 1) {
-              console.log("Player 1 is the winner")
-              return true;
-            } else if((board[0][2].getValue() === 1 && board[1][2].getValue() === 1) && board[2][2].getValue() === 1) {
-              console.log("Player 2 is the winner")
-              return true;
-            }
-
-            // diagonal 1
-            if((board[0][0].getValue() === 1 && board[1][1].getValue() === 1) && board[2][2].getValue() === 1) {
-              console.log("Player 1 is the winner")
-              return true;
-            } else if((board[0][0].getValue() === 1 && board[1][1].getValue() === 1) && board[2][2].getValue() === 1) {
-              console.log("Player 2 is the winner")
-              return true;
-            }            
-            
-            // diagonal 2
-            if((board[2][0].getValue() === 1 && board[1][1].getValue() === 1) && board[0][2].getValue() === 1) {
-              console.log("Player 1 is the winner")
-              return true;
-            } else if((board[2][0].getValue() === 1 && board[1][1].getValue() === 1) && board[0][2].getValue() === 1) {
-              console.log("Player 2 is the winner")
-              return true;
-            }
-          }          
-    
-    return {getBoard, placeToken, printBoard, winningCondition};
+  return { getBoard, placeToken, printBoard, winningCondition };
 }
 
 function Cell() {
-    let value = 0;
+  let value = 0;
 
-    const addToken = (player) => {
-        value = player;
-    };
+  const addToken = (player) => {
+    value = player;
+  };
 
-    const getValue = () => value;
+  const getValue = () => value;
 
-    return {
-        addToken,
-        getValue
-    }
+  return {
+    addToken,
+    getValue,
+  };
 }
 
-function GameController (
-    playerOneName = "Player One",
-    playerTwoName = "Player Two"
+function GameController(
+  playerOneName = "Player One",
+  playerTwoName = "Player Two"
 ) {
-    const board = GameBoard();
+  const board = GameBoard();
 
-    const players = [
-        {
-            name:playerOneName,
-            token: 1
-        },
-        {
-            name: playerTwoName,
-            token:2
-        }
-    ];
+  const players = [
+    {
+      name: playerOneName,
+      token: 1,
+    },
+    {
+      name: playerTwoName,
+      token: 2,
+    },
+  ];
 
-    let activePlayer = players[0];
-    
-    const switchPlayerTurn = () => {
-        activePlayer = activePlayer === players[0] ? players[1] : players[0];
-      };
+  let activePlayer = players[0];
 
-    const getActivePlayer = () => activePlayer;
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  };
 
-    const printNewRound = () => {
-        board.printBoard();
-        console.log(`${getActivePlayer().name}'s turn.`);
-      };
-      
-      const playRound = (row, col) => {
-        board.placeToken(row, col, getActivePlayer().token);
-        switchPlayerTurn();
-        printNewRound();
-      };
+  const getActivePlayer = () => activePlayer;
 
-    // possible coordinate you can input
-    // from 0 to 2 for row
-    // from 0 to 2 for column
-    // there is probably only max 
-    for(let i = 0; i <= 9; i++) {
-        // for now
-        // If one array is filled in the 2D array, then return winner. Horizontal Check
-        // Diagonal Check
-        // Vertical Check
-        // maybe create a new function called checkWinningCondition?
-      const inputRow = prompt("What row number you want to input?"); 
-      const inputCol = prompt("What col number you want to input?");
+  const printNewRound = () => {
+    board.printBoard();
+    console.log(`${getActivePlayer().name}'s turn.`);
+  };
 
-      playRound(inputRow, inputCol);
+  const playRound = (row, col) => {
+    board.placeToken(row, col, getActivePlayer().token);
+    switchPlayerTurn();
+    printNewRound();
+  };
 
-      if(board.winningCondition() == true) {
-        break;
-      }
+  // possible coordinate you can input
+  // from 0 to 2 for row
+  // from 0 to 2 for column
+  // there is probably only max
+  for (let i = 0; i <= 9; i++) {
+    const inputRow = prompt("What row number you want to input?");
+    const inputCol = prompt("What col number you want to input?");
 
-      // this is for the tie condition
-      // might be a bug here
-      if(board.winningCondition() == false || i === 9) {
-        console.log("Tie! Neither player wins!")
-        break;
-      }
-       
+    playRound(inputRow, inputCol);
 
+    if (board.winningCondition() == true) {
+      break;
     }
 
-    printNewRound();
+    // this is for the tie condition
+    // might be a bug here
+    if (board.winningCondition() == false || i === 9) {
+      console.log("Tie! Neither player wins!");
+      break;
+    }
+  }
 
-    return {
-        playRound,
-        getActivePlayer
-      };
-    
+  printNewRound();
+
+  return {
+    playRound,
+    getActivePlayer,
+  };
 }
-
-
 
 const game = GameController();
